1f4c766d4ab68f8631c2a18859f4d357c2ff342b
JeanPi22 <jean.photo.grafico@gmail.com>
2022-08-17 20:16:39 -0500

    Prueba Git Banco

---
+++ 2_Poo_Banco/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+
+//Un banco tiene la necesidad de desarrollar un sistema que le permita a 3 clientes ingresar su nombre,
+//hacer depósitos, hacer retiros, calcular la cantidad de dinero que hay depositada en total en el banco
+//y lo que le corresponde a cada cliente de ese total.
+
+namespace _2_Poo_Banco
+{
+    
+    class Banco
+    {
+        List<User> user = new List<User>();
+        public void addName()
+        {
+            //string name = "";
+            Console.WriteLine("Ingrese su nombre");
+            user.Add(new User(name));
+        }
+
+        public void menu ()
+        {
+            int options;
+
+            Console.WriteLine("Selecciones una opción");
+            Console.WriteLine("1 - Usuario");
+            Console.WriteLine("2 - Registrar usuario");
+            Console.WriteLine("3 - Salir");
+            options = int.Parse(Console.ReadLine());
+
+            while (options!=1 || options!=2)
+            {
+                if (options==1)
+                {
+
+                }
+                else
+                {
+                    addName();
+                    break;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Banco bank = new Banco();
+            bank.menu();
+        }
+
+
+    }   
+  
+}
---
+++ 2_Poo_Banco/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _2_Poo_Banco
+{
+    class User
+    {
+        public string name { get; set; }
+
+        public User(string _name)
+        {
+            this.name = _name;
+        }
+
+        ~User()
+        {
+
+        }
+    }
+}
