9a58d74c6c9191d1c3971dd05efa926703bf33af
JeanPi22 <jean.photo.grafico@gmail.com>
2022-08-17 21:39:52 -0500

    Practica clase

--- 2_Poo_Banco/Program.cs
+++ 2_Poo_Banco/Program.cs
@@ -7,47 +7,94 @@ using System.Collections.Generic;
 
 namespace _2_Poo_Banco
 {
-    
-    class Banco
+    class Client
     {
-        List<User> user = new List<User>();
-        public void addName()
+        public string name; //Nombre del cliente
+        private int amount; //Monto del cliente
+
+        public Client(string names)
         {
-            //string name = "";
-            Console.WriteLine("Ingrese su nombre");
-            user.Add(new User(name));
+            name = names;
+            amount = 0;
         }
 
-        public void menu ()
+        public void ToDeposit (int amounts) //Deposito
         {
-            int options;
+            amount += amounts;
+        }
 
-            Console.WriteLine("Selecciones una opción");
-            Console.WriteLine("1 - Usuario");
-            Console.WriteLine("2 - Registrar usuario");
-            Console.WriteLine("3 - Salir");
-            options = int.Parse(Console.ReadLine());
+        public void Withdraw (int withdraws) //Retirar
+        {
+            amount -= withdraws;
+        }  
+        
+        public void ShowAmount () //Mostrar monto
+        {
+            Console.Write("El dinero en la cuenta es: " + amount);
+        }
 
-            while (options!=1 || options!=2)
+        class Bank
+        {
+            Client client1, client2, client3;
+
+            public Bank()
             {
-                if (options==1)
-                {
-
-                }
-                else
-                {
-                    addName();
-                    break;
-                }
+                client1 = new Client("Jean");
+                client2 = new Client("Pierre");
+                client3 = new Client("Juan");
             }
-        }
-        static void Main(string[] args)
-        {
-            Banco bank = new Banco();
-            bank.menu();
-        }
 
+            static void Main(string[] args)
+            {
+                Bank bnk = new Bank();
+                Console.WriteLine("Nombre del cliente 1: " + bnk.client1.name);
+                Console.WriteLine("Nombre del cliente 2: " + bnk.client2.name);
+                Console.WriteLine("Nombre del cliente 3: " + bnk.client3.name);
 
-    }   
-  
-}
+                bnk.client1.ShowAmount(); //Mostrar monto
+                bnk.client1.ToDeposit(200); //Deporitar monto
+                bnk.client1.ShowAmount(); //Mostrar monto
+            }
+        }        
+    }
+
+    //class Banco
+    //{
+    //    List<User> user = new List<User>();
+    //    public void addName()
+    //    {
+    //        //string name = "";
+    //        Console.WriteLine("Ingrese su nombre");
+    //        user.Add(new User(name));
+    //    }
+
+    //    public void menu ()
+    //    {
+    //        int options;
+
+    //        Console.WriteLine("Selecciones una opción");
+    //        Console.WriteLine("1 - Usuario");
+    //        Console.WriteLine("2 - Registrar usuario");
+    //        Console.WriteLine("3 - Salir");
+    //        options = int.Parse(Console.ReadLine());
+
+    //        while (options!=1 || options!=2)
+    //        {
+    //            if (options==1)
+    //            {
+
+    //            }
+    //            else
+    //            {
+    //                addName();
+    //                break;
+    //            }
+    //        }
+    //    }
+    //    static void Main(string[] args)
+    //    {
+    //        Banco bank = new Banco();
+    //        bank.menu();
+    //    }
+
+}   
--- 2_Poo_Banco/User.cs
+++ 2_Poo_Banco/User.cs
@@ -1,21 +1,21 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Text;
 
-namespace _2_Poo_Banco
-{
-    class User
-    {
-        public string name { get; set; }
+//namespace _2_Poo_Banco
+//{
+//    class User
+//    {
+//        public string name { get; set; }
 
-        public User(string _name)
-        {
-            this.name = _name;
-        }
+//        public User(string _name)
+//        {
+//            this.name = _name;
+//        }
 
-        ~User()
-        {
+//        ~User()
+//        {
 
-        }
-    }
-}
+//        }
+//    }
+//}
